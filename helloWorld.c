a3853f6bf5649ab08a8b8bb981cdf7704a7498e7
Ido Sokolovsky <ido2267@gmail.com>
2019-03-26 20:50:04 +0200

    Update the hello world application

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,10 @@
+
+#include "/platform_independent/tracer.h"
+
 int main()
 {
-   // printf() displays the string inside quotation
+   // printf() displays the string inside 
+     TRACE( "Hello worlf program", __LINE__ , 0, 0);
    printf("Hello, World!");
    return 0;
 }
